f510fdf447c1b202187f05cf1a286749fc10b623
mohamed attia <devm3aatya@gmail.com>
2021-11-10 17:24:45 +0300

    catalog

--- SShop.Web/Features/Catalog/CatalogController.cs
+++ SShop.Web/Features/Catalog/CatalogController.cs
@@ -174,14 +174,14 @@ namespace SShop.Web.Features.Catalog
 
             if (raiseProductPriceChangedEvent) // Save product's data and publish integration event through the Event Bus if price has changed
             {
-                //Create Integration Event to be published through the Event Bus
-                var priceChangedEvent = new ProductPriceChangedIntegrationEvent(catalogItem.Id, productToUpdate.Price, oldPrice);
+                ////Create Integration Event to be published through the Event Bus
+                //var priceChangedEvent = new ProductPriceChangedIntegrationEvent(catalogItem.Id, productToUpdate.Price, oldPrice);
 
-                // Achieving atomicity between original Catalog database operation and the IntegrationEventLog thanks to a local transaction
-                await _catalogIntegrationEventService.SaveEventAndCatalogContextChangesAsync(priceChangedEvent);
+                //// Achieving atomicity between original Catalog database operation and the IntegrationEventLog thanks to a local transaction
+                //await _catalogIntegrationEventService.SaveEventAndCatalogContextChangesAsync(priceChangedEvent);
 
-                // Publish through the Event Bus and mark the saved event as published
-                await _catalogIntegrationEventService.PublishThroughEventBusAsync(priceChangedEvent);
+                //// Publish through the Event Bus and mark the saved event as published
+                //await _catalogIntegrationEventService.PublishThroughEventBusAsync(priceChangedEvent);
             }
             else // Just save the updated product because the Product's Price hasn't changed.
             {
